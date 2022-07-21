16f0de4a1caaaa57eb59f38eed3dc5765d5e8fdf
Ojmyers <106256260+Ojmyers@users.noreply.github.com>
2022-07-21 14:52:58 -0400

    Update appstart.sh

--- appstart.sh
+++ appstart.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-rm -rf /etc/yum.repos.d/nodesource-el*
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
 yum install nodejs --enablerepo=nodesource
-npm install
