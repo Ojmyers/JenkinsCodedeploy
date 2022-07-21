8ea80f3e30b1056438ba93645dd7378fa8d8e0bc
Ojmyers <106256260+Ojmyers@users.noreply.github.com>
2022-07-21 15:23:02 -0400

    Update appstart.sh

--- appstart.sh
+++ appstart.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo yum install -y gcc-c++ make 
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash - 
-npm install /
+sudo amazon-linux-extras install epel
+sudo yum install nodejs npm
