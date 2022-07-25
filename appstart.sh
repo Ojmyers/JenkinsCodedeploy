4489ca93baa4028c221d69891330156443e07149
Ojmyers <106256260+Ojmyers@users.noreply.github.com>
2022-07-25 09:38:05 -0400

    Update appstart.sh

--- appstart.sh
+++ appstart.sh
@@ -1,9 +1,1 @@
 #!/bin/bash
-echo $PATH
-pwd
-cd /var/www/html
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-npm install
-npm start
