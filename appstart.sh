c8fd3146feb8ba67ef5b0ce6e39f3c293b25cd67
Ojmyers <106256260+Ojmyers@users.noreply.github.com>
2022-07-25 11:09:16 -0400

    Update appstart.sh

--- appstart.sh
+++ appstart.sh
@@ -2,10 +2,6 @@
 echo $PATH
 pwd
 cd /var/www/html
--export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 rm -rf node_modules
 npm install
-nvm use v16.13.2
 npm start
